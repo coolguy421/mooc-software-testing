7705e7e9cbcff844b84abd6faa8bb64b067ad4b4
coolgu421y <dsandeep987@gmail.com>
2020-10-11 20:01:01 +0530

    Rectify Ghappy Class bug and added tests

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,27 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    @ParameterizedTest(name = "String - {0}")
+    @CsvSource({"abcggdh", "AggBCDEggF, yuppingg"})
+    public void validInputs(String input){
+        GHappy gHappyObj = new GHappy();
+
+        boolean result = gHappyObj.gHappy(input);
+        Assertions.assertTrue(result);
+    }
+
+    @ParameterizedTest(name = "String - {0}")
+    @CsvSource({"gabcggEh", "abcdeggfhg"})
+    public void gAtStartandEndPositions(String input){
+        GHappy gHappyObj = new GHappy();
+
+        boolean result = gHappyObj.gHappy(input);
+        Assertions.assertFalse(result);
+    }
+
 }
